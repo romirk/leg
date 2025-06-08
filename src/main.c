31cb143625aaf545460610c8c272c3c1b9358bad
romir kulshrestha <romir.kulshrestha@gmail.com>
2025-06-08 08:02:13 +0200

    restore fdt parse

--- src/main.c
+++ src/main.c
@@ -10,11 +10,16 @@
 #include "stdio.h"
 #include "uart.h"
 #include "utils.h"
+#include "fdt/parser.h"
 
 [[noreturn]]
 void kmain() {
     disable_interrupts();
     setup_exceptions();
+
+    auto result = parse_fdt();
+    info("memory start: 0x%p | memory size: 0x%x", result.addr, result.size);
+
     auto cpsr = read_cpsr();
     info("Interrupts masked: %t\n"
            "Fast interrupts masked: %t\n"
@@ -22,9 +27,6 @@ void kmain() {
            cpsr.I, cpsr.F, cpsr.A);
     auto sctlr = read_sctlr();
     info("VE: %t", sctlr.VE);
-
-    auto periphbase = read_periphbase_39_15();
-    info("Periphbase: %x", periphbase);
 
     warn("HALT");
     limbo();
