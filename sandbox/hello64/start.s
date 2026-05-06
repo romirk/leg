bd796cc207377e2674f11a3bde0c62ec9defb0f0
romir kulshrestha <romir.kulshrestha@gmail.com>
2026-05-06 21:26:33 +0200

    A2

--- sandbox/hello64/start.s
+++ sandbox/hello64/start.s
@@ -1,9 +1,18 @@
 .global _start
 
 .section .text
+
+.equ UARTDR, 0x09000000
+.macro putc, c
+    mov x1, \c
+    strb w1, [x0]
+.endm
+
 _start:
-    mov x0, #0x1234
-    mov x1, #0x5678
+    mov  x0, #UARTDR
+    putc #'H'
+    putc #'i'
+    putc #'\n'
 .L.loop:
     wfi
     b .L.loop
