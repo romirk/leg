45c83ec86ce1ed61d5c7ae4333c15d43af3459a6
romir kulshrestha <romir.kulshrestha@gmail.com>
2026-05-06 22:32:15 +0200

    A3

--- sandbox/hello64/start.s
+++ sandbox/hello64/start.s
@@ -8,11 +8,36 @@
     strb w1, [x0]
 .endm
 
-_start:
+print_el:
     mov  x0, #UARTDR
-    putc #'H'
-    putc #'i'
+    mrs  x1, CurrentEL
+    and  x1, x1, #0b1100
+    lsr  x1, x1, #2
+    add  x1, x1, #'0'
+    putc x1
     putc #'\n'
+    ret
+
+_start:
+    bl print_el   
+
+    mrs  x1, hcr_el2
+    orr  x1, x1, #(1 << 31)
+    msr  hcr_el2, x1
+
+    mov  x1, #0x3C5
+    msr  spsr_el2, x1  
+
+    adr x1, el1_main
+    msr elr_el2, x1
+
+    dsb sy
+    isb sy
+    eret
+
+el1_main:
+    bl print_el
+
 .L.loop:
     wfi
     b .L.loop
